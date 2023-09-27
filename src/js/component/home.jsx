28d78cc355cfa4186a094964b28c6b9d57f764c0
Bforti <139017911+Bforti@users.noreply.github.com>
2023-09-27 00:26:21 +0000

    commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,21 +4,17 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="text-center bg-black text-light d-flex justify-content-center m-1">
+			<h1 className="border border-4 rounded m-2 p-3 fa-regular fa-clock"></h1>
+			<h1 className="border border-4 rounded m-2 p-3">{props.five}</h1>
+			<h1 className="border border-4 rounded m-2 p-3">{props.four}</h1>
+			<h1 className="border border-4 rounded m-2 p-3">{props.three}</h1>
+			<h1 className="border border-4 rounded m-2 p-3">{props.two}</h1>
+			<h1 className="border border-4 rounded m-2 p-3">{props.one}</h1>
+			
+			
 		</div>
 	);
 };
